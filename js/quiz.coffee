c474ac833787663d7bf05b992b277006ea73ad94
spirulence <spirulence@gmail.com>
2014-05-03 18:16:07 -0500

    loads of quiz questions

--- js/quiz.coffee
+++ js/quiz.coffee
@@ -5,13 +5,12 @@ quizController = ($scope) ->
     {
       name: "The IT Department"
       questions: [
-        {text: "Are there two or more people in your IT department?"}
-        {text: "Do the people in your IT department work well together?"}
-        {text: "Is the head of your IT department cool-headed and calm, especially under pressure?"}
-        {text: "Does your IT department work remotely less than 20% of the time?"}
+        {text: "Are there two or more people on your IT support staff?"}
+        {text: "Do your IT professionals work well together?"}
+        {text: "Is your head IT professional cool-headed and calm, especially under pressure?"}
+        {text: "Do your IT professionals work remotely less than 20% of the time?"}
         {text: "Do your IT professionals take breaks during the day?"}
         {text: "Is the IT office neat and organized?"}
-        {text: "Do your IT professionals have programming experience?"}
         {text: "Are your IT professionals expected to dress at the same level as other employees?"}
         {text: "Are your IT professionals working in IT because they enjoy it?"}
         {text: "Do your IT professionals use language appropriate for an office?"}
@@ -20,7 +19,7 @@ quizController = ($scope) ->
     {
       name: "Interacting with Users"
       questions: [
-        {text: "Do your IT professionals do daily rounds?"}
+        {text: "Do your IT professionals do weekly rounds?"}
         {text: "Are your IT professionals courteous?"}
         {text: "Do your IT professionals have the respect of your staff?"}
         {text: "Do your IT professionals have the respect of building staff?"}
@@ -37,15 +36,66 @@ quizController = ($scope) ->
         {text: "Are issues that require use of a user's computer worked on only when that user is absent?"}
         {text: "Are issues worked on in priority order?"}
         {text: "Can users easily see information about issues that affect them?"}
+        {text: "Can management easily see a list of open issues?"}
       ]
     }
     {
-      name: "Interacting with Upper Management"
+      name: "IT Projects"
       questions: [
-        {text: "Can management easily see a list of open issues?"}
+        {text: "Does every upgrade come with a cost/benefit analysis?"}
         {text: "Does every project come with a business case?"}
-        {text: "Does every project, once deployed, have a way to gauge effectiveness?"}
+        {text: "Do projects include using beta-testers inside the organization?"}
         {text: "Do projects use some form of change control?"}
+        {text: "Does every project, once deployed, have a way to gauge effectiveness?"}
+      ]
+    }
+    {
+      name: "Network Storage"
+      questions: [
+        {text: "Does your organization use some form of networked storage?"}
+        {text: "Does the networked storage have room for data growth?"}
+        {text: "Is the networked storage organized?"}
+        {text: "Is the network storage backed up on-site?"}
+        {text: "Is the network storage backed up off-site?"}
+        {text: "Are backups encrypted?"}
+        {text: "Is restoring data from backups regularly tested?"}
+      ]
+    }
+    {
+      name: "Maintenance"
+      questions: [
+        {text: "Does your organization use anti-virus software?"}
+        {text: "Do your IT professionals do quarterly maintenance of all PCs?"}
+        {text: "In quarterly maintenance, are PCs (computer, monitor, keyboard) physically cleaned?"}
+        {text: "In quarterly maintenance, are PCs (computer, monitor, keyboard) physically cleaned?"}
+      ]
+    }
+    {
+      name: "Company Policies and Plans"
+      questions: [
+        {text: "Does your company have an Internet Use Policy?"}
+        {text: "Is the Internet Use Policy enforced?"}
+        {text: "Do your IT professionals maintain a Disaster Plan?"}
+        {text: "Does your company have a policy on using personal devices for work?"}
+      ]
+    }
+    {
+      name: "Infrastructure"
+      questions: [
+        {text: "Do your IT professionals maintain a wireless network for guests and clients?"}
+        {text: "Is the wireless network secured with a password?"}
+        {text: "Do your IT professionals keep track of company hardware on loan to users?"}
+        {text: "Do your IT professionals keep loanable hardware organized and in good condition?"}
+        {text: "When old computers are retired, do your IT professionals wipe all company data from them?"}
+
+      ]
+    }
+    {
+      name: "IT Professional Qualifications"
+      questions: [
+        {text: "Do your IT professionals have significant experience with Microsoft Office products?"}
+        {text: "Do your IT professionals have significant experience with the operating systems you use?"}
+        {text: "Do your IT professionals have programming experience?"}
       ]
     }
   ]
--- js/quiz.js
+++ js/quiz.js
@@ -10,19 +10,17 @@
         name: "The IT Department",
         questions: [
           {
-            text: "Are there two or more people in your IT department?"
+            text: "Are there two or more people on your IT support staff?"
           }, {
-            text: "Do the people in your IT department work well together?"
+            text: "Do your IT professionals work well together?"
           }, {
-            text: "Is the head of your IT department cool-headed and calm, especially under pressure?"
+            text: "Is your head IT professional cool-headed and calm, especially under pressure?"
           }, {
-            text: "Does your IT department work remotely less than 20% of the time?"
+            text: "Do your IT professionals work remotely less than 20% of the time?"
           }, {
             text: "Do your IT professionals take breaks during the day?"
           }, {
             text: "Is the IT office neat and organized?"
-          }, {
-            text: "Do your IT professionals have programming experience?"
           }, {
             text: "Are your IT professionals expected to dress at the same level as other employees?"
           }, {
@@ -35,7 +33,7 @@
         name: "Interacting with Users",
         questions: [
           {
-            text: "Do your IT professionals do daily rounds?"
+            text: "Do your IT professionals do weekly rounds?"
           }, {
             text: "Are your IT professionals courteous?"
           }, {
@@ -63,19 +61,94 @@
             text: "Are issues worked on in priority order?"
           }, {
             text: "Can users easily see information about issues that affect them?"
+          }, {
+            text: "Can management easily see a list of open issues?"
           }
         ]
       }, {
-        name: "Interacting with Upper Management",
+        name: "IT Projects",
         questions: [
           {
-            text: "Can management easily see a list of open issues?"
+            text: "Does every upgrade come with a cost/benefit analysis?"
           }, {
             text: "Does every project come with a business case?"
           }, {
-            text: "Does every project, once deployed, have a way to gauge effectiveness?"
+            text: "Do projects include using beta-testers inside the organization?"
           }, {
             text: "Do projects use some form of change control?"
+          }, {
+            text: "Does every project, once deployed, have a way to gauge effectiveness?"
+          }
+        ]
+      }, {
+        name: "Network Storage",
+        questions: [
+          {
+            text: "Does your organization use some form of networked storage?"
+          }, {
+            text: "Does the networked storage have room for data growth?"
+          }, {
+            text: "Is the networked storage organized?"
+          }, {
+            text: "Is the network storage backed up on-site?"
+          }, {
+            text: "Is the network storage backed up off-site?"
+          }, {
+            text: "Are backups encrypted?"
+          }, {
+            text: "Is restoring data from backups regularly tested?"
+          }
+        ]
+      }, {
+        name: "Maintenance",
+        questions: [
+          {
+            text: "Does your organization use anti-virus software?"
+          }, {
+            text: "Do your IT professionals do quarterly maintenance of all PCs?"
+          }, {
+            text: "In quarterly maintenance, are PCs (computer, monitor, keyboard) physically cleaned?"
+          }, {
+            text: "In quarterly maintenance, are PCs (computer, monitor, keyboard) physically cleaned?"
+          }
+        ]
+      }, {
+        name: "Company Policies and Plans",
+        questions: [
+          {
+            text: "Does your company have an Internet Use Policy?"
+          }, {
+            text: "Is the Internet Use Policy enforced?"
+          }, {
+            text: "Do your IT professionals maintain a Disaster Plan?"
+          }, {
+            text: "Does your company have a policy on using personal devices for work?"
+          }
+        ]
+      }, {
+        name: "Infrastructure",
+        questions: [
+          {
+            text: "Do your IT professionals maintain a wireless network for guests and clients?"
+          }, {
+            text: "Is the wireless network secured with a password?"
+          }, {
+            text: "Do your IT professionals keep track of company hardware on loan to users?"
+          }, {
+            text: "Do your IT professionals keep loanable hardware organized and in good condition?"
+          }, {
+            text: "When old computers are retired, do your IT professionals wipe all company data from them?"
+          }
+        ]
+      }, {
+        name: "IT Professional Qualifications",
+        questions: [
+          {
+            text: "Do your IT professionals have significant experience with Microsoft Office products?"
+          }, {
+            text: "Do your IT professionals have significant experience with the operating systems you use?"
+          }, {
+            text: "Do your IT professionals have programming experience?"
           }
         ]
       }
